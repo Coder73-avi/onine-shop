e9fdcc2e3d5f900255857c77d24ef55ce99e97ac
Hunter <hunter@localhost.localdomain>
2022-10-06 11:58:34 +0545

    changes saved

--- controllers/axios.js
+++ controllers/axios.js
@@ -1,5 +1,10 @@
 import axios from "axios";
 
-const instance = axios.create({ baseURL: "http://localhost:4001" });
+const url = process.env.NODEURL || "http://localhost:4001"
+
+const instance = axios.create({
+  baseURL: url,
+  withCredentials: true,
+});
 
 export default instance;
