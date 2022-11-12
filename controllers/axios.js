944430952c8f5d2d53ff00ba73ca29c61da1114a
hunter <hunter@localhost.localdomain>
2022-11-12 16:18:34 +0545

    add cookies with authorization token

--- controllers/axios.js
+++ controllers/axios.js
@@ -1,9 +1,14 @@
 import axios from "axios";
+import { GetCookie } from "./SetCookie";
 
 const url = process.env.URL || "http://localhost:4001";
+const auth = GetCookie("auth");
 const instance = axios.create({
   baseURL: url,
   withCredentials: true,
+  headers: {
+    Authorization: auth,
+  },
 });
 
 export default instance;
